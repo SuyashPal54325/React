d6ab30a1b94b949e4654ad53ef6345ed8662776c
SuyashPal54325 <suyashpal54325@gmail.com>
2025-03-03 10:27:43 +0530

    Understanding of Custom Recat

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,9 +2,11 @@ import Chai from "./Chai"
 
 function App() {
 
+  const username ="Suyash Pal "
+
   return (
     <>
-    <h1>Chai aur react with vite </h1>
+    <h1>Chai aur react with vite {username }</h1>
     <Chai/>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react';
+
 import App from './App.jsx'
 
+// function Myapp(){
+//   return(
+//     <div>
+//       <h4>Customm Increase </h4>
+//     </div>
+//   )
+// }
+
+// const reactElement={
+//   type: 'a',
+//   props:{
+//       href:'https://google.com',
+//       target:'_blank'
+//   },
+//   children:'Click to go google '
+// }
+
+// const anotherElement =(
+//   <a href="https://google.com " target='_blank'>Visit Google </a>
+// )
+
+// const reactElement =React.createElement(
+//   'a',
+//   {href:'https://google.com',target:'_blank'},
+//   'Click me to visit Google '
+// )
+
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <App/>
+  </StrictMode>
+  
 )
