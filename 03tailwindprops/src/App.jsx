573211e3fd6d9272d821bc82443c38d9eecb2f16
SuyashPal54325 <suyashpal54325@gmail.com>
2025-03-03 18:39:26 +0530

    Understanding of Tailwind CSS and Props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from './Components/Card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <>
+   <h1 className='bg-green-400  text-black p-5 rounded-xl'>Tial wind CSS </h1>
+   <Card username  ="Chai aur Code" btnText="Text me "/>
+   <Card  username ="Suyash Pal" btnText="Visit me "/>
+
+   </>
+  )
+}
+
+export default App
